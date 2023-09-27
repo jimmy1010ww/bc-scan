d314770686eae2ad9afdd570545db30d987b154f
jimmy <jimmy1010ww@gmail.com>
2023-09-27 22:37:26 +0800

    Finish task result sync to page  Changes to be committed: 	modified:   .gitignore 	deleted:    logs/app.log 	modified:   main.py 	deleted:    result.txt 	deleted:    results/jimmy/Ballot_result.txt 	deleted:    results/jimmy/Number1_result.txt 	new file:   scan_results_list.pickle 	modified:   static/css/index.css 	modified:   templates/index.html 	modified:   templates/scan_result.html

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import logging
 import os
 import subprocess
 from datetime import datetime, date
+import pickle
 from multiprocessing import Process
 
 main = Blueprint('main', __name__)
@@ -16,8 +17,10 @@ logger = logging.getLogger('app_log')
 
 current_result_id = -1
 
-scan_results = [
-]
+
+scan_results = []
+with open('scan_results_list.pickle', 'wb') as file:
+    pickle.dump(scan_results, file)
 
 empty_scan_result = {
     "id": 0,
@@ -30,9 +33,12 @@ empty_scan_result = {
     "task_result": "None"
 }
 
-def create_task(id, task_status, task_name, task_type, task_filename, task_date, task_duration_time, task_result, wasm_filepath, output_filepath):
-    global current_result_id
+def update_scan_results():
     global scan_results
+    with open('scan_results_list.pickle', 'rb') as file:
+        scan_results = pickle.load(file)
+
+def create_task(id, task_status, task_name, task_type, task_filename, task_date, task_duration_time, task_result, wasm_filepath, output_filepath):
     
     scan_results.append(
         {
@@ -47,23 +53,26 @@ def create_task(id, task_status, task_name, task_type, task_filename, task_date,
         }
     )
     
+    with open('scan_results_list.pickle', 'wb') as file:
+        pickle.dump(scan_results, file)
     
     
     logger.info("id: %s" % id)
-    logger.info("task_status: %s" % scan_results[0]["task_status"])
-    logger.info("task_filename: %s" % scan_results[0]["task_filename"])
-    logger.info("task_name: %s" % scan_results[0]["task_name"])
-    logger.info("task_type: %s" % scan_results[0]["task_type"])
-    logger.info("task_date: %s" % scan_results[0]["task_date"])
+    logger.info("task_status: %s" % scan_results[id]["task_status"])
+    logger.info("task_filename: %s" % scan_results[id]["task_filename"])
+    logger.info("task_name: %s" % scan_results[id]["task_name"])
+    logger.info("task_type: %s" % scan_results[id]["task_type"])
+    logger.info("task_date: %s" % scan_results[id]["task_date"])
     
     
-    process = Process(target=exec_wana, args=(wasm_filepath, output_filepath, task_type))
-    process.start()        
-                    
+    process = Process(target=exec_wana, args=(id, wasm_filepath, output_filepath, task_type))
+    process.start()
 
 
-def exec_wana(wasm_filepath, output_filepath, task_type='EOSIO'):
+
+def exec_wana(id, wasm_filepath, output_filepath, task_type='EOSIO'):
     try:
+
         command = []
         
         # check task type
@@ -79,14 +88,53 @@ def exec_wana(wasm_filepath, output_filepath, task_type='EOSIO'):
             # python3 wana.py --sol -e ethereum_contract.wasm
             command = ['python3', 'WANA/wana.py', '--sol', '-o', str(output_filepath), '-e', str(wasm_filepath)]
         
-
+        # record execute time
+        
+        start_time = datetime.now()
+        
         try:
             logger.info("executing command: %s" % command)
             subprocess.run(command, check=True)
         except subprocess.CalledProcessError as e:
             result = "Error executing command: {}".format(e)
-
-        return result
+        
+        end_time = datetime.now()
+        
+        duration_time = end_time - start_time
+        
+        with open('scan_results_list.pickle', 'rb') as file:
+            scan_results = pickle.load(file)
+        
+        scan_results[int(id)]["task_status"] = "Finished"
+        scan_results[int(id)]["task_duration_time"] = str(duration_time)
+                
+        scan_report = {
+            "fake-eos": False,
+            "forged-transfer-notification": False,
+            "block-dependency": False,
+            "delegate-call": False,
+            "greedy": False,
+            "mishandled-exception": False,
+            "reentrancy": False,
+            "no-vulnerability": False
+        }
+        
+        with open(output_filepath+'.pickle', 'rb') as file:
+            scan_report = pickle.load(file)
+        
+        for key, value in scan_report.items():
+            if value == True:
+                scan_results[int(id)]["task_result"] = key
+                break
+        
+        
+        with open('scan_results_list.pickle', 'wb') as file:
+            pickle.dump(scan_results, file)
+        
+        print(scan_results)
+        
+       
+        
     except Exception as e:
         logger.error("Error: %s" % e)
         return "Error: %s" % e
@@ -120,19 +168,20 @@ def upload_file():
             # 確認是否有上傳檔案              
             if file.filename != '':
                 uploading = True  # 開始上傳
-                    
+                
+                now = datetime.now()
+                str_time = now.strftime("%d/%m/%Y %H:%M:%S")
+                file_str_time = now.strftime("%d%m%Y%H%M%S")
+                
                 # 儲存檔案到指定位置
                 wasm_filepath = os.path.join(UPLOAD_FOLDER, username, file.filename)
-                result_filepath = os.path.join(RESULT_FOLDER, username, task_name+'_result.txt')
+                result_filepath = os.path.join(RESULT_FOLDER, username, task_name+'_'+file_str_time+'_result')
                 file.save(wasm_filepath)
                 
                 logger.info("received file upload.")
                 logger.info("save file at %s" % wasm_filepath)
                 
-                now = datetime.now()
-                
-                
-                create_task(current_result_id + 1, "Scanning", task_name, task_type, filename, str(now.strftime("%d/%m/%Y %H:%M:%S")), "Scanning", "Scanning", wasm_filepath, result_filepath)
+                create_task(current_result_id + 1, "Scanning", task_name, task_type, filename, str(str_time), "Scanning", "Scanning", wasm_filepath, result_filepath)
                 current_result_id += 1
         else:
             print("no file")
@@ -147,7 +196,8 @@ def profile():
 @main.route('/task')
 @login_required
 def task():
-    logger.info(current_result_id)
+    update_scan_results()
+    
     if current_result_id != -1:
         return render_template('task.html', task=scan_results[0], task_result=scan_results, scan_result=render_template("scan_result.html", task=scan_results[0], streamed=True))
     else:
@@ -174,6 +224,28 @@ def get_task_num():
     except:
         data = {
             "value": 0
+        }
+    
+    return jsonify(data)
+
+@main.route('/get_task_status/<id>', methods=['GET'])
+@login_required
+def get_task_status(id):
+    update_scan_results()
+        
+        
+    print(scan_results)
+    try:
+        data = {
+            "task_status": scan_results[int(id)]["task_status"],
+            "task_result": scan_results[int(id)]["task_result"],
+            "task_duration_time": scan_results[int(id)]["task_duration_time"]
+        }
+    except:
+        data = {
+            "task_status": "scanning",
+            "task_result": "scanning",
+            "task_duration_time": "scanning"
         }
     
     return jsonify(data)
